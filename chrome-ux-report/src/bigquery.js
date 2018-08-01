39d283345e28898440839cbc59bcf7d5fb36c5ae
Minhaz Kazi <vlookup@google.com>
2018-08-01 11:45:28 -0700

    Chrome User Experience Community Connector (#48)

---
+++ chrome-ux-report/src/bigquery.js
@@ -0,0 +1,144 @@
+/**
+ * bqlocal object mimics the BigQuery service provided in Apps Script.
+ * This help to switch out the service account or reuse BigQuery related
+ * code from other projects.
+ * 
+ * For full reference on BigQuery service and API See
+ * https://developers.google.com/apps-script/advanced/bigquery and
+ * https://cloud.google.com/bigquery/docs/reference/rest/v2/
+ */
+var bqlocal = bqlocal || {};
+
+bqlocal.setOAuthToken = function(oAuthToken) {
+  this.oAuthToken = oAuthToken;
+};
+
+/**
+ * A generalized function to run mimic Jobs.query and Jobs.getQueryResults
+ * for the BigQuery Apps Scrpit Service
+ *
+ * @param {object} bqRequest See https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query#request-body
+ * @param {string} projectId Project Id for the BigQuery project
+ * @param {string} jobId Id for the BigQuery job
+ * @returns {object} See https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/getQueryResults#response
+ */
+bqlocal.bdQuery = function(bqRequest, projectId, jobId) {
+  var urlElements = [
+    "https://www.googleapis.com/bigquery/v2/projects/",
+    projectId,
+    "/queries"
+  ];
+  if (jobId) {
+    urlElements.push("/" + jobId);
+  }
+  var url = urlElements.join("");
+
+  var responseOptions = {
+    headers: {
+      Authorization: "Bearer " + bqlocal.oAuthToken
+    },
+    method: "post",
+    contentType: "application/json",
+    muteHttpExceptions: true
+  };
+
+  if (bqRequest) {
+    responseOptions.payload = JSON.stringify(bqRequest);
+  }
+
+  var response = UrlFetchApp.fetch(url, responseOptions);
+
+  var queryResults = JSON.parse(response.getContentText());
+  return queryResults;
+};
+
+bqlocal.Jobs = {};
+
+
+/**
+ * See https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
+ */
+bqlocal.Jobs.query = function(bqRequest, projectId) {
+  var queryResults = bqlocal.bdQuery(bqRequest, projectId, null);
+  return queryResults;
+};
+
+
+/**
+ * See https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/getQueryResults
+ */
+bqlocal.Jobs.getQueryResults = function(projectId, jobId) {
+  var queryResults = bqlocal.bdQuery(null, projectId, jobId);
+  return queryResults;
+};
+
+
+/**
+ * Gets data from the Chrome UX dataset for the given url.
+ * Originl code from: https://developers.google.com/apps-script/advanced/bigquery#run_query
+ *
+ * @param {string} url URL/Origin to fetch data for
+ * @returns {object} An object with data and column headers for the given url.
+ */
+function getBqData(url) {
+  var bqRequest = {
+    query: crux.queryString,
+    queryParameters: [
+      {
+        parameterType: {
+          type: "STRING"
+        },
+        parameterValue: {
+          value: url
+        },
+        name: "url"
+      }
+    ],
+    useLegacySql: false
+  };
+
+  var bqClient = JSON.parse(propStore.get("script", "bigQuery.client"));
+  var bqOauthService = getOauthService(bqClient);
+  var bqOAuthToken = bqOauthService.getAccessToken();
+  bqlocal.setOAuthToken(bqOAuthToken);
+
+  var projectId = bqClient.projectId;
+
+  var queryResults = bqlocal.Jobs.query(bqRequest, projectId);
+  var jobId = queryResults.jobReference.jobId;
+
+  // Check on status of the Query Job.
+  var sleepTimeMs = 250;
+  while (!queryResults.jobComplete) {
+    Utilities.sleep(sleepTimeMs);
+    sleepTimeMs *= 2;
+    queryResults = bqlocal.Jobs.getQueryResults(projectId, jobId);
+  }
+
+  // Get all the rows of results.
+  var rows = queryResults.rows;
+  while (queryResults.pageToken) {
+    queryResults = bqlocal.Jobs.getQueryResults(projectId, jobId, {
+      pageToken: queryResults.pageToken
+    });
+    rows = rows.concat(queryResults.rows);
+  }
+
+  var headers = queryResults.schema.fields.map(function(field) {
+    return field.name;
+  });
+
+  var data = new Array(rows.length);
+  for (var i = 0; i < rows.length; i++) {
+    var cols = rows[i].f;
+    data[i] = new Array(cols.length);
+    for (var j = 0; j < cols.length; j++) {
+      data[i][j] = cols[j].v;
+    }
+  }
+
+  return {
+    headers: headers,
+    data: data
+  };
+}
---
+++ chrome-ux-report/src/firebase.js
@@ -0,0 +1,101 @@
+/**
+ * Returns the URL for a file in a firebase database.
+ *
+ * @param {string} projectId The project Id for the firebase database
+ * @param {string} fileName The filename in the database
+ * @returns {string} The url for the file in the database
+ */
+function buildFbUrl(projectId, fileName) {
+  if (fileName) {
+    fileName = "/" + fileName;
+  }
+  var urlElements = [
+    "https://",
+    projectId,
+    ".firebaseio.com/",
+    "origins",
+    fileName,
+    ".json"
+  ];
+  var url = urlElements.join("");
+  return url;
+}
+
+/**
+ * Depending on whether data was retrived from BigQuery, updates the Firebase
+ * cache or reads from the Firebase cache.
+ *
+ * @param {object} origin Contains information about the origin
+ * @param {object} [origin.data] Data fetched from BigQuery for the origin
+ * @param {string} origin.url Origin URL
+ * @param {string} origin.key Hashed key generated from the Origin URL
+ */
+function processFirebase(origin) {
+  var fbClient = JSON.parse(propStore.get("script", "firebase.client"));
+  var fbOauthService = getOauthService(fbClient);
+  var fbOAuthToken = fbOauthService.getAccessToken();
+  var fbUrl = buildFbUrl(fbClient.projectId, origin.key);
+  // If we fetched data from BigQuery delete and rewrite firebase cache
+  // else write data to firebase cache
+  if (origin.data) {
+    console.log("deleting firebase cache for " + origin.url);
+    fbCache("delete", fbOAuthToken, fbUrl);
+    console.log("saving firebase cache for " + origin.url);
+    fbCache("post", fbOAuthToken, fbUrl, origin.data);
+    propStore.set("script", origin.key, origin.lastUpdate);
+  } else {
+    console.log("hitting firebase cache for " + origin.url);
+    origin.data = fbCache("get", fbOAuthToken, fbUrl);
+  }
+}
+
+/**
+ * Generic method for handling the Firebase REST API. 
+ * For `get`: It returns the data at the given url.
+ * For `post`: It posts the data in in Firebase db at the given url and returns `undefined`.
+ * For `delete`: It deletes the data at the given url and returns `undefined`.
+ *
+ * @param {string} method Method for the REST API: `get`, `post`, or `delete`
+ * @param {string} oAuthToken Token for the OAuth client
+ * @param {string} url REST endpoint
+ * @param {string} [originData] Data to be stored for `post` method
+ * @returns {undefined|object} Returns data from the REST endpoint for `get`
+ *          method. For other methods, returns `undefined`.
+ */
+function fbCache(method, oAuthToken, url, originData) {
+  var responseOptions = {
+    headers: {
+      Authorization: "Bearer " + oAuthToken
+    },
+    method: method,
+    contentType: "application/json"
+  };
+
+  // Add payload for post method
+  if (method === "post") {
+    responseOptions["payload"] = JSON.stringify(originData);
+  }
+
+  var response = UrlFetchApp.fetch(url, responseOptions);
+
+  // Return value only for `get`.
+  if (method === "get") {
+    var responseObject = JSON.parse(response);
+    // Firebase realtime db automatically adds a unique key.
+    // Due to our lock service, that will be the only key.
+    var autoKey = Object.keys(responseObject)[0];
+    var returnValue = responseObject[autoKey];
+    return returnValue;
+  }
+}
+
+/**
+ * Deletes the entire Firebase cache
+ *
+ */
+function fbFlushCache() {
+  var fb = processOauth("firebase.client");
+  var fbUrl = buildFbUrl(fb.projectId);
+  console.log("flushing firebase cache");
+  fbCache("delete", fb.token, fbUrl);
+}
