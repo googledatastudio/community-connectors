792db4a9fe822c76850b439ea28a4151a3b37636
Grant <grant@kempnet.com>
2018-08-13 01:22:23 +0100

    feature: added Page Speed Insights Data Studio connector which I used as a learning exercise to learn the api. Additionally I really found it useful to have it in Google Analytics - so it was useful for our office to have the page speed score displayed for any key pages that we were making changes to - so the whole team could keep an ieye on it and report it.
    Since this is a Google owned product- I assume (and hope) others in Google/ the web performance / analytics community would like to add more features to it - such as :
    1. breaking out mobile and desktop score into separate metrics
    2. Listing the rules that are not complied with as a separate set of dimensions to be displayed in a table.
    3. Other cool ideas to help improve speed of websites.
    
    Any feedback - feel free to add issue or tweet me on [@ukdatageek](https://twitter.com/ukdatageek)

---
+++ pagespeed-insights/main.js
@@ -0,0 +1,145 @@
+//NOTE: You can obtain a Google PAge Speed Insights API Key from here: https://developers.google.com/speed/docs/insights/v4/first-app
+function getAuthType() {
+    return {
+        "type": "KEY"
+    };
+}
+
+function isAuthValid() {
+    var userProperties = PropertiesService.getUserProperties();
+    var key = userProperties.getProperty('dscc.key');
+    var isValid = validateKey(key);
+    return isValid
+}
+
+function getConfig(request) {
+    var config = {
+        configParams: [
+            //TODO: Keep this in in case this is a simpler way to show apikey and give users ability to generate one
+            //      {
+            //        name: 'apiKey',
+            //        displayName: 'Api Key',
+            //        helpText: 'Enter the Api Key to use to access Page Speed. Get one from: https://developers.google.com/speed/docs/insights/v4/first-app',
+            //        placeholder: '1231232132131232'
+            //      },
+            {
+                name: 'urlTotest',
+                displayName: 'Url to generate a Page Speed Insights Score',
+                helpText: 'Enter the webpage url to get the Page Speed.',
+                placeholder: 'http://www.yourdomain.com/page'
+            }
+        ]
+    };
+    return config;
+}
+
+var fixedSchema = [
+
+    {
+        name: 'pageSpeed',
+        label: 'Page Speed Insights Score',
+        dataType: 'NUMBER',
+        semantics: {
+            conceptType: 'METRIC',
+            semanticType: 'NUMBER',
+            isReaggregatable: true
+        },
+        defaultAggregationType: 'AVG'
+    },
+    {
+        name: "weburl",
+        label: "weburlName",
+        description: "The uri of the website being analysed by Page Speed Insights",
+        dataType: "STRING",
+        semantics: {
+            conceptType: "DIMENSION",
+            semanticType: 'TEXT'
+        }
+    }
+];
+
+function getSchema(request) {
+    return { schema: fixedSchema };
+}
+
+function getData(request) {
+
+    // Create schema for requested fields
+    var requestedSchema = request.fields.map(function(field) {
+        for (var i = 0; i < fixedSchema.length; i++) {
+            if (fixedSchema[i].name == field.name) {
+                return fixedSchema[i];
+            }
+        }
+    });
+    // Fetch and parse data from API
+    var userProperties = PropertiesService.getUserProperties();
+    var key = userProperties.getProperty('dscc.key');
+
+    var urlparts = [
+        'https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=',
+        request.configParams.urlTotest,
+        '&strategy=mobile&key=',
+        key,
+        '&fields=ruleGroups'
+    ];
+    var url = urlparts.join('');
+    var response = UrlFetchApp.fetch(url);
+    var response = UrlFetchApp.fetch(url);
+    var parsedResponse = JSON.parse(response);
+
+    var values = []
+
+    requestedSchema.forEach(function(field) {
+        switch (field.name) {
+            case 'weburl':
+                var urltoTest = request.configParams.urlTotest;
+                values.push(urltoTest)
+                break
+            case 'pageSpeed':
+                var pageSpeed = parsedResponse.ruleGroups.SPEED.score
+                values.push(pageSpeed)
+                break
+            default:
+                values.push('');
+                break
+        }
+    })
+    requestedData = [{ values: values }];
+    return {
+        schema: requestedSchema,
+        rows: requestedData
+    };
+}
+
+function isAdminUser() {
+    return false
+}
+
+function setCredentials(request) {
+    var key = request.key;
+    var validCreds = validateKey(key);
+    if (!validCreds) {
+        return {
+            errorCode: "INVALID_CREDENTIALS"
+        };
+    }
+    var userProperties = PropertiesService.getUserProperties();
+    userProperties.setProperty("dscc.key", key);
+    return {
+        errorCode: "NONE"
+    };
+}
+
+function resetAuth() {
+    var userProperties = PropertiesService.getUserProperties();
+    userProperties.deleteProperty("dscc.key");
+}
+
+function validateKey(key) {
+    if (key == "" || key == null) {
+        return false
+    } else {
+        return true
+    }
+}
