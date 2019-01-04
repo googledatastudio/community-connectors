1b9ae3c3a1157b9909b675d96f6a11390ddd548d
marc-clickinsight <marc.soares@clickinsight.ca>
2019-01-04 15:09:22 -0500

    Migrate to apps script service

--- community-connectors/where-the-iss-at/main.js
+++ community-connectors/where-the-iss-at/main.js
@@ -1,154 +1,110 @@
 function getConfig(request) {
-  var config = {
-    configParams: [
-      {
-        type: "INFO",
-        name: "connect",
-        text: "This connector does not require any configuration. Click CONNECT at the top right to get started."
-      }
-    ]
-  };
-  return config;
+
+  var cc = DataStudioApp.createCommunityConnector();
+  var config = cc.getConfig();
+
+  config.newInfo()
+  .setId("connect")
+  .setText("This connector does not require any configuration. Click CONNECT at the top right to get started.")
+
+  return config.build();
 };
 
-var fixedSchema = [  
-  {
-    name: 'name',
-    label: 'Satellite Name',
-    dataType: 'STRING',
-    semantics: {
-      conceptType: 'DIMENSION'
-    }
-  },
-  {
-    name: 'id',
-    label: 'Satellite ID',
-    dataType: 'NUMBER',
-    semantics: {
-      conceptType: 'DIMENSION'
-    }
-  },  
-  {
-    name: 'latitude',
-    label: 'Latitude',
-    dataType: 'NUMBER',
-    semantics: {
-      conceptType: 'DIMENSION'
-    }
-  },
-  {
-    name: 'longitude',
-    label: 'Longitude',
-    dataType: 'NUMBER',
-    semantics: {
-      conceptType: 'DIMENSION'
-    }
-  }, 
-  {
-    name: 'latLong',
-    label: 'Latitude, Longitude',
-    dataType: 'STRING',
-    semantics: {
-      conceptType: 'DIMENSION'
-    }
-  },   
-  {
-    name: 'altitude',
-    label: 'Altitude',
-    dataType: 'NUMBER',
-    semantics: {
-      conceptType: 'DIMENSION'
-    }
-  },
-  {
-    name: 'velocity',
-    label: 'Velocity',
-    dataType: 'NUMBER',
-    semantics: {
-      conceptType: 'DIMENSION'
-    }
-  }, 
-  {
-    name: 'visibiltiy',
-    label: 'Visibility',
-    dataType: 'NUMBER',
-    semantics: {
-      conceptType: 'DIMENSION'
-    }
-  },
-  {
-    name: 'footprint',
-    label: 'Footprint',
-    dataType: 'NUMBER',
-    semantics: {
-      conceptType: 'DIMENSION'
-    }
-  },  
-  {
-    name: 'timestamp',
-    label: 'Timestamp',
-    dataType: 'NUMBER',
-    semantics: {
-      conceptType: 'DIMENSION'
-    }
-  },  
-  {
-    name: 'daynum',
-    label: 'Day Number',
-    dataType: 'NUMBER',
-    semantics: {
-      conceptType: 'DIMENSION'
-    }
-  },  
-  {
-    name: 'solar_lat',
-    label: 'Solar Latitude',
-    dataType: 'NUMBER',
-    semantics: {
-      conceptType: 'DIMENSION'
-    }
-  },  
-  {
-    name: 'solar_lon',
-    label: 'Solar Longitude',
-    dataType: 'NUMBER',
-    semantics: {
-      conceptType: 'DIMENSION'
-    }
-  },  
-  {
-    name: 'units',
-    label: 'Units',
-    dataType: 'STRING',
-    semantics: {
-      conceptType: 'DIMENSION'
-    }
-  },
-  {
-    name: 'count',
-    label: 'Count',
-    dataType: 'NUMBER',
-    semantics: {
-      conceptType: 'METRIC',
-      isReaggregatable:true
-    }
-  }  
-];
+function getFields() {
+  var cc = DataStudioApp.createCommunityConnector();
+  var fields = cc.getFields();
+  var types = cc.FieldType;
+  var aggregations = cc.AggregationType;
+
+fields.newDimension()
+    .setId("name")
+    .setName("Satellite Name")
+    .setType(types.TEXT);
+
+fields.newDimension()
+    .setId("id")
+    .setName("Satellite ID")
+    .setType(types.NUMBER);
+
+fields.newDimension()
+    .setId("latitude")
+    .setName("Latitude")
+    .setType(types.NUMBER);
+
+fields.newDimension()
+    .setId("longitude")
+    .setName("Longitude")
+    .setType(types.NUMBER);
+
+fields.newDimension()
+    .setId("latLong")
+    .setName("Latitude, Longitude")
+    .setType(types.LATITUDE_LONGITUDE);
+
+fields.newDimension()
+    .setId("altitude")
+    .setName("Altitude")
+    .setType(types.NUMBER);
+
+fields.newDimension()
+    .setId("velocity")
+    .setName("Velocity")
+    .setType(types.NUMBER);
+
+fields.newDimension()
+    .setId("visibiltiy")
+    .setName("Visibility")
+    .setType(types.NUMBER);
+
+fields.newDimension()
+    .setId("footprint")
+    .setName("Footprint")
+    .setType(types.NUMBER);
+
+fields.newDimension()
+    .setId("timestamp")
+    .setName("Timestamp")
+    .setType(types.NUMBER);
+
+fields.newDimension()
+    .setId("daynum")
+    .setName("Day Number")
+    .setType(types.NUMBER);
+
+fields.newDimension()
+    .setId("solar_lat")
+    .setName("Solar Latitude")
+    .setType(types.NUMBER);
+
+fields.newDimension()
+    .setId("solar_lon")
+    .setName("Solar Longitude")
+    .setType(types.NUMBER);
+
+fields.newDimension()
+    .setId("units")
+    .setName("Units")
+    .setType(types.TEXT);
+
+fields.newMetric()
+    .setId("count")
+    .setName("Count")
+    .setType(types.NUMBER);
+
+  return fields;
+};
 
 function getSchema(request) {
-  return {schema: fixedSchema};
-};
+  return {'schema': getFields().build()};
+}
+
 
 function getData(request) {
-  var dataSchema = [];
-  request.fields.forEach(function(field) {
-    for (var i = 0; i < fixedSchema.length; i++) {
-      if (fixedSchema[i].name === field.name) {
-        dataSchema.push(fixedSchema[i]);
-        break;
-      }
-    }
+
+  var requestedFieldIds = request.fields.map(function(field) {
+    return field.name;
   });
+  var requestedFields = getFields().forIds(requestedFieldIds);
 
   if (!Date.now) {
     Date.now = function now() {
@@ -157,11 +113,11 @@ function getData(request) {
   }
 
   var t = Date.now()/1000;
-  
+
   var timestamps = [];
   for(var i=0; i<10; i++) {
     timestamps.push(t);
-    t = t - (60*5);  
+    t = t - (60*5);
   }
 
   var url = [
@@ -173,8 +129,8 @@ function getData(request) {
   var data = [];
   response.forEach(function(item) {
     var values = [];
-    dataSchema.forEach(function(field) {
-      switch(field.name) {
+    requestedFields.asArray().forEach(function(field) {
+      switch(field.getId()) {
         case 'count':
           values.push('1');
           break;
@@ -182,8 +138,8 @@ function getData(request) {
           values.push(item.latitude + ',' + item.longitude)
           break;
         default:
-          if(!!item[field.name]){
-            values.push(item[field.name]);
+          if(!!item[field.getId()]){
+            values.push(item[field.getId()]);
           } else {
             values.push('');
           }
@@ -194,7 +150,7 @@ function getData(request) {
     });
   });
   return {
-    schema: dataSchema,
+    schema: requestedFields.build(),
     rows: data
   };
 };
