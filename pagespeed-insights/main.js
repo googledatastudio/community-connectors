c914b86a455538a00ec95258791fc7ba219924a1
Grant Kemp <grant.kemp@photobox.com>
2019-03-24 02:56:02 +0000

    admin: ran prettier

--- pagespeed-insights/main.js
+++ pagespeed-insights/main.js
@@ -14,20 +14,17 @@ function isAuthValid() {
 
 function getConfig(request) {
   var config = {
-    configParams: [
-      {
-        name: 'urlTotest',
-        displayName: 'Url to generate a Page Speed Insights Score',
-        helpText: 'Enter the webpage url to get the Page Speed.',
-        placeholder: 'https://www.yourdomain.com/page'
-      }
-    ]
+    configParams: [{
+      name: 'urlTotest',
+      displayName: 'Url to generate a Page Speed Insights Score',
+      helpText: 'Enter the webpage url to get the Page Speed.',
+      placeholder: 'https://www.yourdomain.com/page'
+    }]
   };
   return config;
 }
 
-var fixedSchema = [
-  {
+var fixedSchema = [{
     name: 'pageSpeedDesktop',
     label: 'PS Insights Desktop Score',
     dataType: 'NUMBER',
@@ -94,7 +91,9 @@ var fixedSchema = [
 ];
 
 function getSchema(request) {
-  return {schema: fixedSchema};
+  return {
+    schema: fixedSchema
+  };
 }
 /**
  * @param {deviceCat} deviceCategory - Can be 'mobile' or 'desktop' only.
@@ -102,7 +101,7 @@ function getSchema(request) {
 
 function getData(request) {
   // Create schema for requested fields
-  var requestedSchema = request.fields.map(function(field) {
+  var requestedSchema = request.fields.map(function (field) {
     for (var i = 0; i < fixedSchema.length; i++) {
       if (fixedSchema[i].name == field.name) {
         return fixedSchema[i];
@@ -113,29 +112,29 @@ function getData(request) {
   var userProperties = PropertiesService.getUserProperties();
   var key = userProperties.getProperty('dscc.key');
   var urlToTest = request.configParams.urlTotest;
-  
+
   var values = [];
   // Note Would be nice to do this with promises however this doesn't seem to work with Data Studio Connectors - I just get an error
-  var urlMobile = buildUrl('mobile',key,urlToTest)
+  var urlMobile = buildUrl('mobile', key, urlToTest)
   var responseMobile = UrlFetchApp.fetch(urlMobile);
   var parsedResponseMobile = JSON.parse(responseMobile);
-  
+
   // Get Opportunities
- var ruleResultsMobile = parsedResponseMobile.lighthouseResult.audits;
- var opportunitiesMobile = buildOpportunities(ruleResultsMobile);
-  
-  var urlDesktop = buildUrl('desktop',key,urlToTest);
+  var ruleResultsMobile = parsedResponseMobile.lighthouseResult.audits;
+  var opportunitiesMobile = buildOpportunities(ruleResultsMobile);
+
+  var urlDesktop = buildUrl('desktop', key, urlToTest);
   var responseDesktop = UrlFetchApp.fetch(urlDesktop);
   var parsedResponseDesktop = JSON.parse(responseDesktop);
 
-  
+
   // Get Opportunities Desktop
   var ruleResultsDesktop = parsedResponseDesktop.lighthouseResult.audits;
   var opportunitiesDesktop = buildOpportunities(ruleResultsDesktop);
-  
 
- 
-  requestedSchema.forEach(function(field) {
+
+
+  requestedSchema.forEach(function (field) {
     switch (field.name) {
       case 'weburl':
         var urltoTest = request.configParams.urlTotest;
@@ -145,32 +144,34 @@ function getData(request) {
         var pageSpeedDesktop = parsedResponseDesktop.lighthouseResult.categories.performance.score * 100;
         values.push(pageSpeedDesktop);
         break;
-       case 'pageSpeedMobile':
+      case 'pageSpeedMobile':
         var pageSpeedMobile = parsedResponseMobile.lighthouseResult.categories.performance.score * 100;
         values.push(pageSpeedMobile);
         break;
-       case 'OpportunitiesDesktop':
-       var desktopCount = Object.keys(opportunitiesDesktop).length
+      case 'OpportunitiesDesktop':
+        var desktopCount = Object.keys(opportunitiesDesktop).length
         values.push(desktopCount);
         break;
-       case 'OpportunitiesMobile':
-       var mobileCount = Object.keys(opportunitiesMobile).length
+      case 'OpportunitiesMobile':
+        var mobileCount = Object.keys(opportunitiesMobile).length
         values.push(mobileCount);
         break;
-       case 'webReport':
+      case 'webReport':
         var urltoTest = request.configParams.urlTotest;
-        var webReportUrl = "https://developers.google.com/speed/pagespeed/insights/?url="+urltoTest;
+        var webReportUrl = "https://developers.google.com/speed/pagespeed/insights/?url=" + urltoTest;
         values.push(webReportUrl);
         break;
-        
+
       default:
         values.push('');
         break;
     }
   });
-  
-  
-  requestedData = [{values: values}];
+
+
+  requestedData = [{
+    values: values
+  }];
   return {
     schema: requestedSchema,
     rows: requestedData
@@ -216,24 +217,25 @@ function buildUrl(deviceCat, key, urlToTest) {
 }
 
 function buildOpportunities(audits) {
- var opportunityThreshold = 0;
- 
- for (var key in audits) { 
-   if (typeof audits[key].details === "undefined") {
-     delete audits[key]
-   } else {
-     if (audits[key].details.type != "opportunity"|| audits[key].details.overallSavingsMs == opportunityThreshold) {
-       delete audits[key]
-     } 
-   }
- }
- return audits;
+  var opportunityThreshold = 0;
+
+  for (var key in audits) {
+    if (typeof audits[key].details === "undefined") {
+      delete audits[key]
+    } else {
+      if (audits[key].details.type != "opportunity" || audits[key].details.overallSavingsMs == opportunityThreshold) {
+        delete audits[key]
+      }
+    }
+  }
+  return audits;
 }
+
 function validateKey(key) {
   if (key == '' || key == null) {
     return false;
   } else {
     return true;
   }
-  
+
 }
