7b3ba3f9a6d418a3500adf02ec0d98359857ff4a
Mike <michal.paczoski@nf.pl>
2019-05-07 16:15:29 +0200

    yarn prettier

--- senuto/src/code.js
+++ senuto/src/code.js
@@ -5,14 +5,13 @@ and use the datas from SENUTO API in Data Studio.
 
 var connector = connector || {};
 
-
 /**
  * An enum that defines the authentication types that can be set for a connector.
  * $see https://developers.google.com/apps-script/reference/data-studio/auth-type
  */
 function getAuthType() {
-    var response = {type: 'NONE'}; // OAUTH2, USER_PASS, KEY, USER_TOKEN
-    return response;
+  var response = {type: 'NONE'}; // OAUTH2, USER_PASS, KEY, USER_TOKEN
+  return response;
 }
 
 /**
@@ -21,66 +20,65 @@ function getAuthType() {
  * @see https://developers.google.com/apps-script/reference/data-studio/config
  */
 function getConfig() {
-    var cc = DataStudioApp.createCommunityConnector();
-    var config = cc.getConfig();
+  var cc = DataStudioApp.createCommunityConnector();
+  var config = cc.getConfig();
 
-    config
-        .newTextInput()
-        .setId('hash')
-        .setName('Security ID')
-        .setHelpText('Enter the Security ID to get history.')
-        .setPlaceholder('securityidsecurityidsecurityidsecurityid');
+  config
+    .newTextInput()
+    .setId('hash')
+    .setName('Security ID')
+    .setHelpText('Enter the Security ID to get history.')
+    .setPlaceholder('securityidsecurityidsecurityidsecurityid');
 
-    config
-        .newTextInput()
-        .setId('domain')
-        .setName('Domain')
-        .setHelpText('Enter the Domain to get history.')
-        .setPlaceholder('senuto.com');
+  config
+    .newTextInput()
+    .setId('domain')
+    .setName('Domain')
+    .setHelpText('Enter the Domain to get history.')
+    .setPlaceholder('senuto.com');
 
-    return config.build();
+  return config.build();
 }
 
-
 var npmSchema = [
-    {
-        name: 'time',
-        dataType: 'STRING',
-        semantics: {
-            conceptType: 'DIMENSION',
-            semanticType: 'YEAR_MONTH_DAY'
-        }
-    },
-    {
-        name: 'top3_history',
-        dataType: 'NUMBER',
-        semantics: {
-            conceptType: 'METRIC',
-            semanticType: 'NUMBER',
-            isReaggregatable: true
-        },
-        defaultAggregationType: 'SUM'
-    },
-    {
-        name: 'top10_history',
-        dataType: 'NUMBER',
-        semantics: {
-            conceptType: 'METRIC',
-            semanticType: 'NUMBER',
-            isReaggregatable: true
-        },
-        defaultAggregationType: 'SUM'
-    },
-    {
-        name: 'top50_history',
-        dataType: 'NUMBER',
-        semantics: {
-            conceptType: 'METRIC',
-            semanticType: 'NUMBER',
-            isReaggregatable: true
-        },
-        defaultAggregationType: 'SUM'
+  {
+    name: 'time',
+    dataType: 'STRING',
+    semantics: {
+      conceptType: 'DIMENSION',
+      semanticType: 'YEAR_MONTH_DAY'
     }
+  },
+  {
+    name: 'top3_history',
+    dataType: 'NUMBER',
+    semantics: {
+      conceptType: 'METRIC',
+      semanticType: 'NUMBER',
+      isReaggregatable: true
+    },
+    defaultAggregationType: 'SUM'
+  },
+  {
+    name: 'top10_history',
+    dataType: 'NUMBER',
+    semantics: {
+      conceptType: 'METRIC',
+      semanticType: 'NUMBER',
+      isReaggregatable: true
+    },
+    defaultAggregationType: 'SUM'
+  },
+  {
+    name: 'top50_history',
+    dataType: 'NUMBER',
+    semantics: {
+      conceptType: 'METRIC',
+      semanticType: 'NUMBER',
+      isReaggregatable: true
+    },
+    defaultAggregationType: 'SUM'
+  }
 ];
 
 /**
@@ -89,9 +87,8 @@ var npmSchema = [
  * @return {object} The schema.
  */
 function getSchema(request) {
-    return {schema: npmSchema};
+  return {schema: npmSchema};
 }
-
 
 /**
  * Gets the data for the community connector
@@ -99,55 +96,64 @@ function getSchema(request) {
  * @return {object} The data.
  */
 function getData(request) {
-    // Create schema for requested fields
-    var requestedSchema = request.fields.map(function (field) {
-        for (var i = 0; i < npmSchema.length; i++) {
-            if (npmSchema[i].name == field.name) {
-                return npmSchema[i];
-            }
-        }
+  // Create schema for requested fields
+  var requestedSchema = request.fields.map(function(field) {
+    for (var i = 0; i < npmSchema.length; i++) {
+      if (npmSchema[i].name == field.name) {
+        return npmSchema[i];
+      }
+    }
+  });
+
+  // Fetch and parse data from API
+  var url = [
+    'https://api.senuto.com/api/data_studio/visibility_analysis/domain_positions/getPositionsHistoryChartData?',
+    ,
+    'domain=',
+    request.configParams.domain,
+    '&date_min=2016-03-01',
+    '&hash=',
+    request.configParams.hash
+  ];
+
+  var response = UrlFetchApp.fetch(url.join(''));
+  var parsedResponse = JSON.parse(response).data;
+
+  // Transform parsed data and filter for requested fields
+  var requestedData = parsedResponse.map(function(schemaData) {
+    var values = [];
+
+    requestedSchema.forEach(function(field) {
+      switch (field.name) {
+        case 'time':
+          values.push(
+            Utilities.formatDate(
+              new Date(schemaData.time * 1000),
+              'Europe/Warsaw',
+              'yyyyMMdd'
+            ).toString()
+          );
+          break;
+        case 'top3_history':
+          values.push(schemaData.top3_history);
+          break;
+        case 'top10_history':
+          values.push(schemaData.top10_history);
+          break;
+        case 'top50_history':
+          values.push(schemaData.top50_history);
+          break;
+        default:
+          values.push('');
+          break;
+      }
     });
 
-    // Fetch and parse data from API
-    var url = [
-        'https://api.senuto.com/api/data_studio/visibility_analysis/domain_positions/getPositionsHistoryChartData?',
-        ,'domain=', request.configParams.domain
-        ,'&date_min=2016-03-01'
-        ,'&hash=',  request.configParams.hash
-    ];
+    return {values: values};
+  });
 
-    var response = UrlFetchApp.fetch(url.join(''));
-    var parsedResponse = JSON.parse(response).data;
-
-    // Transform parsed data and filter for requested fields
-    var requestedData = parsedResponse.map(function ( schemaData) {
-        var values = [];
-
-        requestedSchema.forEach(function (field) {
-            switch (field.name) {
-                case 'time':
-                    values.push((Utilities.formatDate(new Date(schemaData.time * 1000), 'Europe/Warsaw', 'yyyyMMdd')).toString());
-                    break;
-                case 'top3_history':
-                    values.push(schemaData.top3_history);
-                    break;
-                case 'top10_history':
-                    values.push(schemaData.top10_history);
-                    break;
-                case 'top50_history':
-                    values.push(schemaData.top50_history);
-                    break;
-                default:
-                    values.push('');
-                    break;
-            }
-        });
-
-        return {values: values};
-    });
-
-    return {
-        schema: requestedSchema,
-        rows: requestedData
-    };
+  return {
+    schema: requestedSchema,
+    rows: requestedData
+  };
 }
