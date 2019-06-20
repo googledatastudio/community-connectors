28296be0aec7a0168641753ab5e33bd16fb090bc
Gabriël Ramaker <gabriel@lingewoud.nl>
2019-06-20 12:35:56 +0200

    types.YEAR_MONTH_DAY_HOUR should not include minutes and seconds

--- JSON-connect/src/main.js
+++ JSON-connect/src/main.js
@@ -332,9 +332,7 @@ function convertDate(val) {
     date.getUTCFullYear() +
     ('0' + (date.getUTCMonth() + 1)).slice(-2) +
     ('0' + date.getUTCDate()).slice(-2) +
-    ('0' + date.getUTCHours()).slice(-2) +
-    ('0' + date.getUTCMinutes()).slice(-2) +
-    ('0' + date.getUTCSeconds()).slice(-2)
+    ('0' + date.getUTCHours()).slice(-2)
   );
 }
 
