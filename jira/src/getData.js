f4be8104903ec1f40cc8e6781401dc9e1d1c5e7e
Enmanuel Parache <EnmanuelParache@users.noreply.github.com>
2019-11-14 19:29:23 -0300

    Automating field construction.

--- jira/src/getData.js
+++ jira/src/getData.js
@@ -15,127 +15,34 @@ function responseToRows(requestedFields, response, request) {
   // Transform parsed data and filter for requested fields
   return response.map(function(issue) {
     var row = [];
-    var metrics = getIssueMetrics(issue.changelog.histories);
     requestedFields.asArray().forEach(function(field) {
-      switch (field.getId()) {
+      var fieldId = field.getId();
+      switch (fieldId) {
         case 'id':
           return row.push(issue.id);
         case 'url':
           return row.push('https://'+ request.configParams.host +'/browse/' + issue.key);
-        case 'key':
+        case 'issuekey':
           return row.push(issue.key);
-        case 'statuscategorychangedate':
-          return row.push(Utilities.formatDate(new Date(issue.fields.statuscategorychangedate), timeZone, format));
-        case 'resolution':
-          return row.push(issue.fields.resolution ? issue.fields.resolution.name : 'Unresolved');
-        case 'priority':
-          return row.push(issue.fields.priority);
-        case 'labels':
-          return row.push(issue.fields.labels.sort().join());
-        case 'assignee':
-          return row.push(issue.fields.assignee ? issue.fields.assignee.displayName : 'Unassigned');
-        case 'status':
-          return row.push(issue.fields.status.name);
-        case 'statusCategory':
-          return row.push(issue.fields.status.statusCategory.name);
-        case 'components':
-          return row.push(issue.fields.components.map(function(component){ return component.name}).sort().join());
-        case 'storyPoints':
-          return row.push(issue.fields.customfield_14532);
-        case 'actualStoryPoints':
-          return row.push(issue.fields.customfield_11542);
-        case 'creator':
-          return row.push(issue.fields.creator.displayName);
-        case 'reporter':
-          return row.push(issue.fields.reporter.displayName);
-        case 'issueLabel':
-          return row.push(issue.fields.customfield_15608 ? issue.fields.customfield_15608.value : null);
-        case 'issuetype':
-          return row.push(issue.fields.issuetype.name);
-        case 'project':
-          return row.push(issue.fields.project.name);
-        case 'resolutiondate':
-          return row.push(issue.fields.resolutiondate ? Utilities.formatDate(new Date(issue.fields.resolutiondate), timeZone, format) : null);
-        case 'created':
-          return row.push(Utilities.formatDate(new Date(issue.fields.created), timeZone, format));
-        case 'brand':
-          return row.push(issue.fields.customfield_15435 ? issue.fields.customfield_15435.map(function(brand){return brand.value}).join() : null);
-        case 'updated':
-          return row.push(Utilities.formatDate(new Date(issue.fields.updated), timeZone, format));
-        case 'pullRequest':
-          return row.push(issue.fields.customfield_11340);
-        case 'sprint':
-          return row.push(issue.fields.customfield_10140 ? issue.fields.customfield_10140.join().replace(/.*name=|,goal=.*/g,'') : null);
-        case 'description':
-          return row.push(issue.fields.description);
-        case 'brandCollection':
-          return row.push(issue.fields.customfield_15421 ? issue.fields.customfield_15421.value : null);
-        case 'summary':
-          return row.push(issue.fields.summary);
-        case 'points' :
-          return row.push(issue.fields.customfield_10003);
-        case 'duedate':
-          return row.push(issue.fields.duedate ? Utilities.formatDate(new Date(issue.fields.duedate), timeZone, format) : null);
-        case 'daysLastUpdated':
-          return row.push(getDaysSince(issue.fields.updated));
-        case 'daysSinceStatusCategoryChange':
-          return row.push(getDaysSince(issue.fields.statuscategorychangedate));
-        case 'started':
-          return row.push(Utilities.formatDate(new Date(metrics.started), timeZone, format));
-        case 'closed':
-          return row.push(Utilities.formatDate(new Date(metrics.closed), timeZone, format));
-        case 'durationInProgess':
-          return row.push(metrics.seconds);
-        case 'daysInProgress':
-          return row.push(metrics.days);
         default:
-          return row.push('');
+          var field = issue.fields[fieldId] 
+          var result = ''
+          if(field.displayName || field.value || field.name){
+            result = field.displayName || field.value || field.name;
+          }
+          else if(field.join){
+            result = field.join();
+          }
+          else if(field.stringify){
+            result = field.stringify();
+          }
+          return row.push(result);
       }
     });
     return {values: row};
   });
 }
 
-function getDaysSince(dateUpdated){
-  const dayInMilliseconds = 1*24*60*60*1000 //8.64e7
-  const today = new Date()
-  const date = new Date(dateUpdated)
-  
-  return Math.round((today.getTime() - date.getTime())/dayInMilliseconds)
-}
-
-// This function returns an object containing dates a ticket was started and completed plus period in days 
-function getIssueMetrics(histories){
-  const dayInMilliseconds = 1*24*60*60*1000 //8.64e7
-  var startedDate = []
-  var closedDate = []
-    
-  for(var currentHistory in histories){
-    var history = histories[currentHistory]
-    for(var currentItem in history.items){
-      var item = history.items[currentItem]
-
-      if(item.field === 'status'){
-        switch(item.toString){
-          case 'In Progress':
-            startedDate.push(new Date(history.created));
-            break;
-          case 'Closed':
-            closedDate.push(new Date(history.created));
-            break;
-        }
-      }
-    }
-  }
-  var days = (Date.parse(closedDate) - Date.parse(startedDate))/dayInMilliseconds
-  var seconds = (Date.parse(closedDate) - Date.parse(startedDate))/1000
-  return {
-    "started":startedDate, 
-    "closed":closedDate, 
-    "days": isNaN(days) ? 0 : days, 
-    "seconds": isNaN(seconds) ? 0 : seconds
-  }
-}
 /**
  * Return true if valid
  * @param {object} value 
@@ -155,7 +62,8 @@ function getData(request) {
     return field.name;
   });
   
-  var requestedFields = getFields().forIds(requestedFieldIds);
+  var fieldsData = getJiraFields(request);
+  var requestedFields = getFields(fieldsData).forIds(requestedFieldIds);
   
   var jql = [
     request.configParams.dateForQuery != 'none' ? request.configParams.dateForQuery +' >= ' + request.dateRange.startDate + ' AND ' + request.configParams.dateForQuery + ' <= ' + request.dateRange.endDate : '',
--- jira/src/getSchema.js
+++ jira/src/getSchema.js
@@ -3,12 +3,12 @@
  * @return {Fields} The Community Connector fields.
  * @see https://developers.google.com/apps-script/reference/data-studio/fields
  */
-function getFields() {
+function getFields(fieldsData) {
   var cc = DataStudioApp.createCommunityConnector();
   var fields = cc.getFields();
   var types = cc.FieldType;
   var aggregations = cc.AggregationType;
-
+  
   fields.newDimension()
       .setId('id')
       .setName('ID')
@@ -18,171 +18,196 @@ function getFields() {
       .setId('url')
       .setName('Url')
       .setType(types.URL);
-
-  fields.newDimension()
-      .setId('key')
-      .setName('Key')
-      .setType(types.TEXT);
   
-  fields.newDimension()
-      .setId('statuscategorychangedate')
-      .setName('Status category change date')
-      .setType(types.YEAR_MONTH_DAY_HOUR);
-  
-  fields.newDimension()
-      .setId('resolution')
-      .setName('Resolution')
-      .setType(types.TEXT);
-  
-  fields.newDimension()
-      .setId('priotity')
-      .setName('Priority')
-      .setType(types.TEXT);
-  
-  fields.newDimension()
-      .setId('labels')
-      .setName('Labels')
-      .setType(types.TEXT);
+  fieldsData.forEach(
+    function(field){
+      var type = null;
+      var fieldSchema = field.schema ? field.schema.type : null;
+      switch(fieldSchema){
+        case 'datetime':
+          type = types.YEAR_MONTH_DAY_HOUR;
+          break;
+        case 'number':
+          type = types.NUMBER;
+        default:
+          type = types.TEXT;
+      };
       
-  fields.newDimension()
-    .setId('assignee')
-    .setName('Assignee')
-    .setType(types.TEXT);
-    
-  fields.newDimension()
-    .setId('status')
-    .setName('Status')
-    .setType(types.TEXT);
+      if(fieldSchema === 'number'){
+        fields.newMetric()
+        .setId(field.id)
+        .setName(field.name)
+        .setType(type);
+      }
+      else{
+        fields.newDimension()
+        .setId(field.id)
+        .setName(field.name)
+        .setType(type);
+      }
+    }
+  );
   
-  fields.newDimension()
-    .setId('statusCategory')
-    .setName('Status Category')
-    .setType(types.TEXT);
-    
-  fields.newDimension()
-    .setId('components')
-    .setName('Components')
-    .setType(types.TEXT);
-    
-  fields.newDimension()
-    .setId('creator')
-    .setName('Creator')
-    .setType(types.TEXT);
-  
-  fields.newDimension()
-    .setId('reporter')
-    .setName('Reporter')
-    .setType(types.TEXT);
-  
-  fields.newDimension()
-    .setId('issueLabel')
-    .setName('Issue Label')
-    .setType(types.TEXT);
-    
-  fields.newDimension()
-    .setId('issuetype')
-    .setName('Issue type')
-    .setType(types.TEXT);
-  
-  fields.newDimension()
-   .setId('project')
-   .setName('Project')
-   .setType(types.TEXT);
-  
-  fields.newDimension()
-    .setId('resolutiondate')
-    .setName('Resolution Date')
-    .setType(types.YEAR_MONTH_DAY_HOUR);
-  
-  fields.newDimension()
-    .setId('created')
-    .setName('Created')
-    .setType(types.YEAR_MONTH_DAY_HOUR);
-  
-  fields.newDimension()
-   .setId('brand')
-   .setName('Brand')
-   .setType(types.TEXT);
-  
-  fields.newDimension()
-    .setId('updated')
-    .setName('Updated')
-    .setType(types.YEAR_MONTH_DAY_HOUR);
-  
-  fields.newDimension()
-   .setId('pullRequest')
-   .setName('Pull Request')
-   .setType(types.URL);
-  
-  fields.newDimension()
-     .setId('sprint')
-     .setName('Sprint')
-     .setType(types.TEXT);
-  
-  fields.newDimension()
-     .setId('description')
-     .setName('Description')
-     .setType(types.TEXT);
-  
-  fields.newDimension()
-   .setId('brandCollection')
-   .setName('Brand/Collection')
-   .setType(types.TEXT);
-  
-  fields.newDimension()
-   .setId('summary')
-   .setName('Summary')
-   .setType(types.TEXT);
-  
-  fields.newDimension()
-    .setId('duedate')
-    .setName('Due date')
-    .setType(types.YEAR_MONTH_DAY_HOUR);
-  
-  fields.newDimension()
-    .setId('started')
-    .setName('Date started')
-    .setType(types.YEAR_MONTH_DAY_HOUR);
-  
-  fields.newDimension()
-    .setId('closed')
-    .setName('Date closed')
-    .setType(types.YEAR_MONTH_DAY_HOUR);
-  
-  fields.newMetric()
-    .setId('durationInProgress')
-    .setName('Time in progress')
-    .setType(types.DURATION);
-  
-  fields.newMetric()
-    .setId('daysInProgress')
-    .setName('Days in progress')
-    .setType(types.NUMBER);
-  
-  fields.newMetric()
-    .setId('daysLastUpdated')
-    .setName('Days last updated')
-    .setType(types.NUMBER);
-  
-  fields.newMetric()
-    .setId('daysSinceStatusCategoryChange')
-    .setName('Days since last status category changed')
-    .setType(types.NUMBER);
-  
-  fields.newMetric()
-    .setId('points')
-    .setName('Points')
-    .setType(types.NUMBER);
-  
-  fields.newMetric()
-    .setId('storyPoints')
-    .setName('Story Points')
-    .setType(types.NUMBER);
-  
-  fields.newMetric()
-    .setId('actualStoryPoints')
-    .setName('Actual Story Points')
-    .setType(types.NUMBER);
+//  
+//  fields.newDimension()
+//      .setId('statuscategorychangedate')
+//      .setName('Status category change date')
+//      .setType(types.YEAR_MONTH_DAY_HOUR);
+//  
+//  fields.newDimension()
+//      .setId('resolution')
+//      .setName('Resolution')
+//      .setType(types.TEXT);
+//  
+//  fields.newDimension()
+//      .setId('priotity')
+//      .setName('Priority')
+//      .setType(types.TEXT);
+//  
+//  fields.newDimension()
+//      .setId('labels')
+//      .setName('Labels')
+//      .setType(types.TEXT);
+//      
+//  fields.newDimension()
+//    .setId('assignee')
+//    .setName('Assignee')
+//    .setType(types.TEXT);
+//    
+//  fields.newDimension()
+//    .setId('status')
+//    .setName('Status')
+//    .setType(types.TEXT);
+//  
+//  fields.newDimension()
+//    .setId('statusCategory')
+//    .setName('Status Category')
+//    .setType(types.TEXT);
+//    
+//  fields.newDimension()
+//    .setId('components')
+//    .setName('Components')
+//    .setType(types.TEXT);
+//    
+//  fields.newDimension()
+//    .setId('creator')
+//    .setName('Creator')
+//    .setType(types.TEXT);
+//  
+//  fields.newDimension()
+//    .setId('reporter')
+//    .setName('Reporter')
+//    .setType(types.TEXT);
+//  
+//  fields.newDimension()
+//    .setId('issueLabel')
+//    .setName('Issue Label')
+//    .setType(types.TEXT);
+//    
+//  fields.newDimension()
+//    .setId('issuetype')
+//    .setName('Issue type')
+//    .setType(types.TEXT);
+//  
+//  fields.newDimension()
+//   .setId('project')
+//   .setName('Project')
+//   .setType(types.TEXT);
+//  
+//  fields.newDimension()
+//    .setId('resolutiondate')
+//    .setName('Resolution Date')
+//    .setType(types.YEAR_MONTH_DAY_HOUR);
+//  
+//  fields.newDimension()
+//    .setId('created')
+//    .setName('Created')
+//    .setType(types.YEAR_MONTH_DAY_HOUR);
+//  
+//  fields.newDimension()
+//   .setId('brand')
+//   .setName('Brand')
+//   .setType(types.TEXT);
+//  
+//  fields.newDimension()
+//    .setId('updated')
+//    .setName('Updated')
+//    .setType(types.YEAR_MONTH_DAY_HOUR);
+//  
+//  fields.newDimension()
+//   .setId('pullRequest')
+//   .setName('Pull Request')
+//   .setType(types.URL);
+//  
+//  fields.newDimension()
+//     .setId('sprint')
+//     .setName('Sprint')
+//     .setType(types.TEXT);
+//  
+//  fields.newDimension()
+//     .setId('description')
+//     .setName('Description')
+//     .setType(types.TEXT);
+//  
+//  fields.newDimension()
+//   .setId('brandCollection')
+//   .setName('Brand/Collection')
+//   .setType(types.TEXT);
+//  
+//  fields.newDimension()
+//   .setId('summary')
+//   .setName('Summary')
+//   .setType(types.TEXT);
+//  
+//  fields.newDimension()
+//    .setId('duedate')
+//    .setName('Due date')
+//    .setType(types.YEAR_MONTH_DAY_HOUR);
+//  
+//  fields.newDimension()
+//    .setId('started')
+//    .setName('Date started')
+//    .setType(types.YEAR_MONTH_DAY_HOUR);
+//  
+//  fields.newDimension()
+//    .setId('closed')
+//    .setName('Date closed')
+//    .setType(types.YEAR_MONTH_DAY_HOUR);
+//  
+//  fields.newMetric()
+//    .setId('durationInProgress')
+//    .setName('Time in progress')
+//    .setType(types.DURATION);
+//  
+//  fields.newMetric()
+//    .setId('daysInProgress')
+//    .setName('Days in progress')
+//    .setType(types.NUMBER);
+//  
+//  fields.newMetric()
+//    .setId('daysLastUpdated')
+//    .setName('Days last updated')
+//    .setType(types.NUMBER);
+//  
+//  fields.newMetric()
+//    .setId('daysSinceStatusCategoryChange')
+//    .setName('Days since last status category changed')
+//    .setType(types.NUMBER);
+//  
+//  fields.newMetric()
+//    .setId('points')
+//    .setName('Points')
+//    .setType(types.NUMBER);
+//  
+//  fields.newMetric()
+//    .setId('storyPoints')
+//    .setName('Story Points')
+//    .setType(types.NUMBER);
+//  
+//  fields.newMetric()
+//    .setId('actualStoryPoints')
+//    .setName('Actual Story Points')
+//    .setType(types.NUMBER);
     
 //  fields.newMetric()
 //      .setId('downloads')
@@ -198,6 +223,38 @@ function getFields() {
  * @return {object} The schema.
  */
 function getSchema(request) {
-  var fields = getFields().build();
+  var fieldsData = getJiraFields(request);
+  var fields = getFields(fieldsData).build();
   return {'schema': fields};
+}
+
+/**
+ * Builds the Community Connector schema.
+ * @param {object} request The request.
+ * @return {object} Jira fields.
+ */
+function getJiraFields(request){
+  var headers = {
+    "Authorization":"Basic " + Utilities.base64Encode(request.configParams.username + ':' + request.configParams.apiToken)
+  };
+  
+  var params = {
+    "contentType":"application/json",
+    "headers":headers, //Authentication sent as a header
+    "method":'get',
+    "validateHttpsCertificates":false,
+    "followRedirects":true,
+    "muteHttpExceptions":true,
+    "escaping":true
+  };
+  
+  var url = [
+      'https://',
+      request.configParams.host,
+      '/rest/api/2/field?'
+  ];
+    
+    // Fetch and parse data from API
+  var response = UrlFetchApp.fetch(encodeURI(url.join('')), params);
+  return JSON.parse(response);
 }
